6f14cbed313e639f3cafa17f1ad8cc38e2d68848
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2020-01-18 23:31:15 +0200

    first commit

---
+++ json.js
@@ -0,0 +1,26 @@
+let json = `{
+	   	"id": 1,
+        "name": "Проволока низкоуглеродистая Ø 0,4 мм ГОСТ 3282-74 (ОК)",
+        "price": 0.04,
+        "img": "C:/Server/data/htdocs/site-test.com/9.jpg",
+        "url" : "<a href='https://provoloka.inf.ua/product44'><b>Характеристика товара</b></a>",
+        "stock" : "<b style='color:green;'>На складе 10.03 тонн</b>"      
+	}`;
+
+
+// let obj = JSON.stringify(json);
+let data = JSON.parse(json);
+let keysData = Object.entries(data); 
+
+
+console.log(keysData);
+
+let header = '<h2>' + keysData[1][1] + '</h2>';
+let list = '';
+
+for (let i in keysData) {
+  list += '<li>' + keysData[i][1] + ' </li>';
+}
+
+document.getElementById('1').innerHTML += header;
+document.getElementById('1').innerHTML += '<ul>' + list + '</ul>';
---
+++ stock.php
@@ -0,0 +1,3 @@
+<?php	
+echo '<div id="1"></div>'.'<script src="json.js"></script>';
+?>
