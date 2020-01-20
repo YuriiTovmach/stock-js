a33b2ddbab7cebc6333803b52de2590e58f7ea14
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2020-01-20 22:44:59 +0200

    fourh commit

--- json.js
+++ json.js
@@ -3,7 +3,6 @@ document.body.style.backgroundColor = "red"; // body
     let myHeight = document.body.clientHeight;
 		console.log("Width = " + myWidth + " px" +" "+"Height = " + myHeight + " px" ); //размер окна
 
-
 let nav = document.createElement("DIV"); //div - навигация
   nav.className = 'nav-class';
   nav.id = 'nav-id';
@@ -25,32 +24,98 @@ let navUl = document.createElement("UL"); // ul - навигация
   navUl.innerHTML = 'Привет, это UL для header';
   navUl.style.height = 20 + "px";
   navUl.style.width = 500 + "px";
-  navUl.style.marginLeft = 0 + "px";
-  navUl.style.marginRight = 0 + "px";
+  navUl.style.right = 200 + "px";
+  navUl.style.marginTop = -10 + "px";
   navUl.style.backgroundColor = "violet";
   navUl.style.border = "solid";
   navUl.style.borderColor = "black";
+  navUl.style.position = "absolute";
   
   console.log(navUl);
   document.body.appendChild(navUl);
 
 
-  let navLi = document.createElement("LI"); // li - навигация 
-  navLi.className = 'navLi-class';
-  navLi.id = 'navLi-id';
-  navLi.innerHTML = 'Привет, это Li для Ul header';
-  navLi.style.height = 20 + "px";
-  navLi.style.width = 400 + "px";
-  navLi.style.marginLeft = 0 + "px";
-  navLi.style.marginRight = 0 + "px";
-  navLi.style.backgroundColor = "orange";
-  navLi.style.border = "solid";
-  navLi.style.borderColor = "black";
+let navUlInHeader = document.getElementsByClassName("navUl-class")[0]; // вставляем Ul в header
+  nav.appendChild(navUlInHeader);
+
+
+let navLi_1 = document.createElement("LI"); // li_1 - навигация 
+  navLi_1.className = 'navLi_1-class';
+  navLi_1.id = 'navLi_1-id';
+  navLi_1.innerHTML = 'Привет, это Li_1 для Ul header';
+  navLi_1.style.height = 20 + "px";
+  navLi_1.style.width = 400 + "px";
+  navLi_1.style.marginLeft = 0 + "px";
+  navLi_1.style.marginRight = 0 + "px";
+  navLi_1.style.backgroundColor = "orange";
+  navLi_1.style.border = "solid";
+  navLi_1.style.borderColor = "black";
+  navLi_1.style.zIndex = "2";
   
-  console.log(navLi);
-  document.body.appendChild(navLi);
+  
+  console.log(navLi_1);
+  document.body.appendChild(navLi_1);
+
+  let navLi_2 = document.createElement("LI"); // li_2 - навигация 
+  navLi_2.className = 'navLi_2-class';
+  navLi_2.id = 'navLi_2-id';
+  navLi_2.innerHTML = 'Привет, это Li_2 для Ul header';
+  navLi_2.style.height = 20 + "px";
+  navLi_2.style.width = 400 + "px";
+  navLi_2.style.marginLeft = 0 + "px";
+  navLi_2.style.marginRight = 0 + "px";
+  navLi_2.style.backgroundColor = "orange";
+  navLi_2.style.border = "solid";
+  navLi_2.style.borderColor = "black";
+  navLi_2.style.zIndex = "2";
+
+  
+  console.log(navLi_2);
+  document.body.appendChild(navLi_2);
+
+  let navLi_3 = document.createElement("LI"); // li_3 - навигация 
+  navLi_3.className = 'navLi_3-class';
+  navLi_3.id = 'navLi_3-id';
+  navLi_3.innerHTML = 'Привет, это Li_3 для Ul header';
+  navLi_3.style.height = 20 + "px";
+  navLi_3.style.width = 400 + "px";
+  navLi_3.style.marginLeft = 0 + "px";
+  navLi_3.style.marginRight = 0 + "px";
+  navLi_3.style.backgroundColor = "orange";
+  navLi_3.style.border = "solid";
+  navLi_3.style.borderColor = "black";
+  navLi_3.style.zIndex = "2";
+ 
+  
+  console.log(navLi_3);
+  document.body.appendChild(navLi_3);
+
+  let navLi_4 = document.createElement("LI"); // li_4 - навигация 
+  navLi_4.className = 'navLi_4-class';
+  navLi_4.id = 'navLi_4-id';
+  navLi_4.innerHTML = 'Привет, это Li_4 для Ul header';
+  navLi_4.style.height = 20 + "px";
+  navLi_4.style.width = 400 + "px";
+  navLi_4.style.marginLeft = 0 + "px";
+  navLi_4.style.marginRight = 0 + "px";
+  navLi_4.style.backgroundColor = "orange";
+  navLi_4.style.border = "solid";
+  navLi_4.style.borderColor = "black";
+  navLi_4.style.zIndex = "2";
+  
+  console.log(navLi_4);
+  document.body.appendChild(navLi_4);
 
 
+let liInUl_1 = document.getElementsByClassName("navLi_1-class")[0]; // вставляем Li_1 в Ul header
+	navUl.appendChild(liInUl_1);
+let liInUl_2 = document.getElementsByClassName("navLi_2-class")[0]; // вставляем Li_2 в Ul header
+	navUl.appendChild(liInUl_2);
+let liInUl_3 = document.getElementsByClassName("navLi_3-class")[0]; // вставляем Li_3 в Ul header
+	navUl.appendChild(liInUl_3);
+let liInUl_4 = document.getElementsByClassName("navLi_4-class")[0]; // вставляем Li_4 в Ul header
+	navUl.appendChild(liInUl_4);	
+		
 
 
 let sideRight = document.createElement("DIV"); //div - сайдбар правый
@@ -70,7 +135,6 @@ let sideRight = document.createElement("DIV"); //div - сайдбар правы
   document.body.appendChild(sideRight);
 
 
-
 let sideLeft = document.createElement("DIV"); //div - сайдбар левый
   sideLeft.className = 'sideLeft-class';
   sideLeft.id = 'sideLeft-id';
@@ -82,11 +146,9 @@ let sideLeft = document.createElement("DIV"); //div - сайдбар левый
   sideLeft.style.backgroundColor = "blue";
   sideLeft.style.border = "solid";
   sideLeft.style.borderColor = "black";
-
+  
   console.log(sideLeft);
   document.body.appendChild(sideLeft);
-
-
 
 
 let cont = document.createElement("DIV"); //div - контекст
@@ -101,13 +163,9 @@ let cont = document.createElement("DIV"); //div - контекст
   cont.style.backgroundColor = "yellow";
   cont.style.border = "solid";
   cont.style.borderColor = "black";
-
-
+  
   console.log(cont);
-  let cont_1 = document.body.appendChild(cont);
-
-
-
+  document.body.appendChild(cont);
 
 
 let foot = document.createElement("DIV"); //div - футер
@@ -122,59 +180,7 @@ let foot = document.createElement("DIV"); //div - футер
   foot.style.border = "solid";
   foot.style.borderColor = "black";
 
+  
   console.log(foot);
   document.body.appendChild(foot);
 
-
-
-
-// let btn = document.createElement("BUTTON"); //button 
-// btn.innerHTML = "Click me!";
-
-// console.log(btn);
-// let btn_1 = document.body.appendChild(btn);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let json = `{
-// 	   	"id": 1,
-//         "name": "Проволока низкоуглеродистая Ø 0,4 мм ГОСТ 3282-74 (ОК)",
-//         "price": 0.04,
-//         "img": "C:/Server/data/htdocs/site-test.com/9.jpg",
-//         "url" : "<a href='https://provoloka.inf.ua/product44'><b>Характеристика товара</b></a>",
-//         "stock" : "<b style='color:green;'>На складе 10.03 тонн</b>"      
-// 	}`;
-
-
-// // let obj = JSON.stringify(json);
-// let data = JSON.parse(json);
-// let keysData = Object.entries(data); 
-
-
-// console.log(keysData);
-
-// let header = '<h2>' + keysData[1][1] + '</h2>';
-// let list = '';
-
-// for (let i in keysData) {
-//   list += '<li>' + keysData[i][1] + ' </li>';
-// }
-
-// document.getElementById('1').innerHTML += header;
-// document.getElementById('1').innerHTML += '<ul>' + list + '</ul>';
